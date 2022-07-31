e6f79aa0847af0b0c68ab543c0533c765613e24d
Your Name <you@example.com>
2022-07-31 13:30:44 +0600

    empty range images

--- whatsapp.py
+++ whatsapp.py
@@ -6,8 +6,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import file_data
 import asyncio
-from db_data.db_commands import get_data_whatsapp, get_data_message, get_data_message_sku, update_table, get_data_message_send, get_data_message_sku2
-
+from db_data.db_commands import get_data_whatsapp, get_data_message, get_data_message_sku, update_table, \
+    get_data_message_send, get_data_message_sku2
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -17,10 +17,8 @@ async def whatsapp_render(contact, data, driver):
         get = await get_data_message()
         if data[1] in await get_data_message_sku():
             data_sku = get[data[1]]
-            print(data_sku[1])
-            print(get)
             driver.get(contact)
-            await asyncio.sleep(10)
+            await asyncio.sleep(20)
             message_content = f"Здравствуйте,\n Благодарим вас за заказ!"
 
             inp_xpath = '//div[@contenteditable="true"][@data-tab="10"]'
@@ -35,24 +33,19 @@ async def whatsapp_render(contact, data, driver):
             attachment_box.click()
 
             await asyncio.sleep(1)
-            image_box = driver.find_element(By.XPATH, '//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
-            file_path1 = os.path.join(base_dir, str(data_sku[1]))
-            file_path2 = os.path.join(base_dir, str(data_sku[2]))
             h = ""
-            if data_sku[1]:
+            if len(data_sku) >= 2:
+
                 h += os.path.join(base_dir, str(data_sku[1]))
                 for i in data_sku[2:]:
                     h += '\n' + os.path.join(base_dir, str(i))
-            print(h)
-            print((file_path1 + '\n' + file_path2))
-            image_box.send_keys(h)
 
-            await asyncio.sleep(5)
-
-            send_button = driver.find_element(By.XPATH, '//span[@data-icon="send"]')
-
-            send_button.click()
-            print(data[-1])
+            if h != "":
+                image_box = driver.find_element(By.XPATH, '//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+                image_box.send_keys(h)
+                await asyncio.sleep(5)
+                send_button = driver.find_element(By.XPATH, '//span[@data-icon="send"]')
+                send_button.click()
             await update_table(data[-1])
 
         await asyncio.sleep(2)
@@ -63,13 +56,13 @@ async def whatsapp_render(contact, data, driver):
 
 async def messenger(url, data, driver):
     try:
-        contacts = ["700619706"]
+        # contacts = ["700619706"]
         phone = []
         for i in data[0]:
             phone.append(i[0])
         for n in range(len(data)):
-            # contact = url+data[n][0]
-            contact = url+contacts[0]
+            contact = url+data[n][0]
+            # contact = url + contacts[0]
             if str(data[n][1]) in await get_data_message_sku2():
                 await whatsapp_render(contact, data[n], driver)
     except Exception as e:
